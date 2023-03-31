798585247e69de834e809233eeac9e1043d47530
wasim7raja10 <wasim7raja10@gmail.com>
2023-04-01 02:47:32 +0530

    format

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,5 @@
-import app from './server';
+import app from "./server";
 
 app.listen(3000, () => {
-  console.log('Server is listening on port 3000');
+	console.log("Server is listening on port 3000");
 });
--- src/server.ts
+++ src/server.ts
@@ -1,10 +1,10 @@
-import express from 'express';
+import express from "express";
 
 const app = express();
 
-app.get('/', (req, res) => {
-  console.log('Hello from Express!');
-  res.status(200).json({ message: 'Hello from Express!' });
+app.get("/", (req, res) => {
+	console.log("Hello from Express!");
+	res.status(200).json({ message: "Hello from Express!" });
 });
 
 export default app;
