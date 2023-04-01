27925e74b4e32a85234e88ae6b84af334a526863
wasim7raja10 <wasim7raja10@gmail.com>
2023-04-01 16:36:50 +0530

    defined routes

---
+++ src/router.ts
@@ -0,0 +1,32 @@
+import { Router } from "express";
+
+const router = Router();
+
+/**
+ * Product
+ */
+router.get("/product", () => {});
+router.get("/product/:id", () => {});
+router.post("/product", () => {});
+router.put("/product/:id", () => {});
+router.delete("/product/:id", () => {});
+
+/**
+ * Update
+ */
+router.get("/update", () => {});
+router.get("/update/:id", () => {});
+router.post("/update", () => {});
+router.put("/update/:id", () => {});
+router.delete("/update/:id", () => {});
+
+/**
+ * Update Point
+ */
+router.get("/updatepoint", () => {});
+router.get("/updatepoint/:id", () => {});
+router.post("/updatepoint", () => {});
+router.put("/updatepoint/:id", () => {});
+router.delete("/updatepoint/:id", () => {});
+
+export default router;
--- src/server.ts
+++ src/server.ts
@@ -1,4 +1,5 @@
 import express from "express";
+import router from "./router";
 
 const app = express();
 
@@ -6,5 +7,7 @@ app.get("/", (req, res) => {
 	console.log("Hello from Express!");
 	res.status(200).json({ message: "Hello from Express!" });
 });
+
+app.use("/api", router);
 
 export default app;
