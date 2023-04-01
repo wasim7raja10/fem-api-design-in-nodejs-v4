62e336b7a7fca66e0814c5ce8d092d70181fb31d
wasim7raja10 <wasim7raja10@gmail.com>
2023-04-01 17:47:51 +0530

    added morgan

--- src/server.ts
+++ src/server.ts
@@ -1,7 +1,10 @@
 import express from "express";
 import router from "./router";
+import morgan from "morgan";
 
 const app = express();
+
+app.use(morgan("dev"));
 
 app.get("/", (req, res) => {
 	console.log("Hello from Express!");
