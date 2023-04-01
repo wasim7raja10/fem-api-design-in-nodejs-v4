80e07bb74ad92a2a32356e2093b8a3f10e836e68
wasim7raja10 <wasim7raja10@gmail.com>
2023-04-01 18:05:28 +0530

    added cors

--- src/server.ts
+++ src/server.ts
@@ -1,10 +1,15 @@
 import express from "express";
-import router from "./router";
 import morgan from "morgan";
+import cors from "cors";
+
+import router from "./router";
 
 const app = express();
 
+app.use(cors());
 app.use(morgan("dev"));
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
 app.get("/", (req, res) => {
 	console.log("Hello from Express!");
